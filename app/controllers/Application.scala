e040849fabf5a2c9bd1404a8e91a8b701be55866
Mehmet Ali Gözaydın <kubudik@gmail.com>
2013-03-25 00:31:41 +0200

    init message

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,17 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+//Selam olsun defterdar.
+//Bu uygulamanın ufkumu genişlemtesi,
+//bir çocuk edasıyla büyümesi, gelişmesi,
+//en az hayalimdeki kadar güzelleşmesi,
+//ve bu kodu her açtığımda ilk günkü heveste olmam dileğiyle.
+//Haydi bismillah...
+
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Sa everyone."))
   }
   
 }
